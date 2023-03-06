ddcc95e48589e6469ec95353beb81499943d12fd
eersaa <eerik.saarinen@gmail.com>
2023-03-06 11:49:37 +0000

    Initial cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for folder in ls .ipfs-host*; do
+    rm -rf $folder
+done
+
+for i in {1..2}; do
+    mkdir -p .ipfs-host$i/data
+    mkdir -p .ipfs-host$i/export
+done
+
+echo "hello world" > .ipfs-host1/export/hello
