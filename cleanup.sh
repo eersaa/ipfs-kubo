fae23d96d15f9a34ebb5a9bd3cc585fc55a3671f
eersaa <eerik.saarinen@gmail.com>
2023-03-06 12:12:17 +0000

    Clean and create two ipfs-host folder structures

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for folder in ls .ipfs-host*; do
+for folder in ls *ipfs-host*; do
     rm -rf $folder
 done
 
 for i in {1..2}; do
-    mkdir -p .ipfs-host$i/data
-    mkdir -p .ipfs-host$i/export
+    mkdir -p ./ipfs-host$i/data
+    mkdir -p ./ipfs-host$i/export
 done
 
-echo "hello world" > .ipfs-host1/export/hello
+echo "hello world" > ./ipfs-host1/export/hello
